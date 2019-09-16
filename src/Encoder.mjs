7676e2acbbaf8c7dee62c23ee3dc2d2a617bff30
Lee Goddard <leegee@gmail.com>
2019-09-16 14:58:11 +0200

    fluent has a bug

--- src/Encoder.mjs
+++ src/Encoder.mjs
@@ -1,40 +1,56 @@
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-const ffmpeg = require('fluent-ffmpeg');
+const path = require('path');
+const spawn = require('child_process').spawn;
+var stream = require('stream');
 
-ffmpeg.setFfmpegPath(ffmpegPath);
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 
 // thx https://dzone.com/articles/creating-video-on-the-server-in-nodejs
 
 module.exports = class Encoder {
-    static command = ffmpeg();
+    secsPerImage = 0.25;
+    width = 1920;
+    height = 1080;
 
-    encode() {
-        this.timemark = null;
-        // 6 consecutive 1920x1080 frames, held 6 seconds each, 30fps, with m4a audio, watermarked
-        Encoder.command
-            .on('end', this.onEnd.bind(this))
-            .on('progress', this.onProgress(this))
-            .on('error', this.onError(this))
-            .input('assets/demo2/folds-of-spacetime_%03d.png')
-            .inputFPS(1 / 6)
-            .videoFilter(["movie=assets/demo2/soa-watermark.png [watermark]; [in][watermark] overlay=10:main_h-overlay_h-10 [out]"])
-            .input('assets/demo2/folds-of-spacetime.m4a')
-            .output('assets/demo2/folds-of-spacetime.mp4')
-            .outputFPS(30)
-            .run();
+    constructor() {
+        this.imagesStream = new stream.PassThrough();
     }
 
-    onProgress(progress) {
-        if (progress.timemark != this.timemark) {
-            this.timemark = progress.timemark;
-            console.log('Time mark: ' + this.timemark + "...");
-        }
+    init() {
+        return new Promise((resolve, reject) => {
+            const framerate = '1/' + this.secsPerImage;
+            const videosize = `${this.width}x${this.height}`;
+            // var audiotrack = path.join(AUDIO_ROOT, options.audio.track);
+            const outputFilename = path.join(path.join('./output.mp4'));
+
+            var childProcess = spawn(ffmpegPath, ['-y', '-f', 'image2pipe',
+                '-s', videosize,
+                '-framerate', framerate,
+                '-pix_fmt', 'yuv420p',
+                '-i', '-',
+                // '-i', audiotrack,
+                '-vcodec', 'mpeg4',
+                '-shortest',
+                outputFilename
+            ]);
+
+            childProcess.stdout.on('data', data => console.log(data.toString()));
+            childProcess.stderr.on('data', data => console.log(data.toString()));
+            childProcess.on('close', code => {
+                console.log(`Done: (${code})`);
+                resolve();
+            });
+
+            this.imagesStream.pipe(childProcess.stdin);
+        });
     }
-    onError(err, stdout, stderr) {
-        console.log('Cannot process video: ' + err.message);
+
+
+    addImage(buffer) {
+        this.imagesStream.write(buffer, 'utf8');
     }
-    onEnd() {
-        console.log('Finished processing');
+
+    finally() {
+        this.imagesStream.end();
     }
 }
 
