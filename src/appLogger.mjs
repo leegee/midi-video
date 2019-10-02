c743f10c0a44384b04416f75321f44998754aeb9
Lee Goddard <leegee@gmail.com>
2019-10-02 12:14:40 +0200

    logging#

--- src/appLogger.mjs
+++ src/appLogger.mjs
@@ -6,9 +6,9 @@ module.exports = winston.createLogger({
     transports: [
         new (winston.transports.Console)({
             level: process.env.LEVEL || 'info',
-            format: format.combine(
-                format.splat(),
-                format.simple()
+            format: winston.format.combine(
+                winston.format.splat(),
+                winston.format.simple()
             ),
         }),
         new (winston.transports.File)({
@@ -18,26 +18,26 @@ module.exports = winston.createLogger({
     ]
 });
 
-function getStackInfo(stackIndex) {
-    const stacklist = (new Error()).stack.split('\n').slice(3)
+// function getStackInfo(stackIndex) {
+//     const stacklist = (new Error()).stack.split('\n').slice(3)
 
-    const stackReg = /at\s+(.*)\s+\((.*):(\d*):(\d*)\)/gi;
-    const stackReg2 = /at\s+()(.*):(\d*):(\d*)/gi;
+//     const stackReg = /at\s+(.*)\s+\((.*):(\d*):(\d*)\)/gi;
+//     const stackReg2 = /at\s+()(.*):(\d*):(\d*)/gi;
 
-    const s = stacklist[stackIndex] || stacklist[0];
-    const sp = stackReg.exec(s) || stackReg2.exec(s);
+//     const s = stacklist[stackIndex] || stacklist[0];
+//     const sp = stackReg.exec(s) || stackReg2.exec(s);
 
-    if (sp && sp.length === 5) {
-        return sp[1] + ' ' + path.relative(__dirname, sp[2]) + ' ' + sp[3];
-        //   return {
-        //     method: sp[1],
-        //     relativePath: path.relative(__dirname, sp[2]),
-        //     line: sp[3],
-        //     pos: sp[4],
-        //     file: path.basename(sp[2]),
-        //     stack: stacklist.join('\n')
-        //   }
-    }
+//     if (sp && sp.length === 5) {
+//         return sp[1] + ' ' + path.relative(__dirname, sp[2]) + ' ' + sp[3];
+//         //   return {
+//         //     method: sp[1],
+//         //     relativePath: path.relative(__dirname, sp[2]),
+//         //     line: sp[3],
+//         //     pos: sp[4],
+//         //     file: path.basename(sp[2]),
+//         //     stack: stacklist.join('\n')
+//         //   }
+//     }
 
-    return '';
-}
+//     return '';
+// }
