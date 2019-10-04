7273c080c5a6dbc310fd64ce302b88ac1d120c27
Lee Goddard <leegee@gmail.com>
2019-10-04 09:19:22 +0200

    better logger

--- src/appLogger.mjs
+++ src/appLogger.mjs
@@ -1,19 +1,43 @@
-// const path = require( 'path' );
-
+const path = require( 'path' );
+const util = require('util');
 const winston = require('winston');
+
+function transform(info, opts) {
+    const args = info[Symbol.for('splat')];
+    if (args) {
+        info.message = util.format(info.message, ...args);
+    }
+    return info;
+}
+
+function utilFormatter() {
+    return {
+        transform
+    };
+}
 
 module.exports = winston.createLogger({
     transports: [
         new(winston.transports.Console)({
             level: process.env.LEVEL || 'info',
-            format: winston.format.combine(
-                winston.format.splat(),
-                winston.format.simple()
-            ),
+            format: winston.format.combine(winston.format.splat(), winston.format.simple()),
         }),
         new(winston.transports.File)({
-            filename: path.join(process.cwd, 'log.log'),
-            level: 'silly'
+            filename: path.join(process.cwd(), 'log.log'),
+            level: 'silly',
+            format: winston.format.combine(
+                winston.format.timestamp({
+                    format: 'YYYY-MM-DD HH:mm:ss.SSS'
+                }),
+                utilFormatter(), // <-- this is what changed
+                winston.format.colorize(),
+                winston.format.printf(({
+                    level,
+                    message,
+                    label,
+                    timestamp
+                }) => `${timestamp} ${label || '-'} ${level}: ${message}`),
+            )
         })
     ]
 });
